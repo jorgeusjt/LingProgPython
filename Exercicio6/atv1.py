5ce7c661d361e42bf2cbc57031aa0c0624bf8218
usjt2016 <jorgelimass@hotmail.com>
2018-10-02 22:31:21 -0300

    subir

--- Exercicio6/atv1.py
+++ Exercicio6/atv1.py
@@ -1,36 +1,3 @@
-from logging import exception
-class Contato:
-    def __init__ (self, nome, numero):
-        self.nome = nome
-        self.numero = numero
-    def __str__ (self):
-    return f'Nome: {self.nome}, Número: {self.numero}'
-
-
-    def cadastro_de_contato ():
-        menu = "1-Cadastrar\n2-Listar\n0-Sair\n"
-        
-      op = int (input (menu))
-    if op == 1:
-    try:
-        f = open ("agenda.txt","a")
-        nome = input("Digite o nome")
-        numero = input("Digite o numero")
-        f.write(nome + ": " + numero "\n")
-        f.close()
-        
-    except IOError:
-        print('Não deu certo')
-    finally:
-        print('Realmente passou por aqui')
-    
-    if op == 2:
-        
-        
-    if op == 3:
-    
-    
-    
-    
-    
-   
+def soma_nat (n):
+        return 1 if n == 1 else n + soma_nat(n - 1)
+print(soma_nat(2), soma_nat(1), soma_nat(5))
